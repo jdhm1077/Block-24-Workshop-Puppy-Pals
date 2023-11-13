fac0ce5e2943353e098f1e2d06c369ce0f3e6cbe
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-13 15:46:31 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-     
+      {puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   }) }
       </div>
      
     </>
