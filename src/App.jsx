2290354cb987792d2a2344738e24ea6e23ae33a4
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-13 17:07:35 -0500

    rendering puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,18 @@ function App() {
     <>
       <div>
       {puppies.map((puppy) => {
+        const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+        console.log(featuredPup)
      return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
      {()=>{console.log("puppy id: ", puppy.id)}}
    }) }
-   {featPupId && <p>{featPupId}</p>}
+   {featPupId &&  <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+     </div>}
       </div>
      
     </>
